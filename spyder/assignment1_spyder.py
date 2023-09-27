aaf195826d9ad7b33df46dcb07d80428425bf157
Tarushi7 <146152415+Tarushi7@users.noreply.github.com>
2023-09-27 22:49:02 +0800

    Add files via upload

---
+++ spyder/assignment1_spyder.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 22:36:41 2023
+
+@author: sushii
+"""
+integers = 12345678910
+print(integers)
+
+my_string = '1 2 3 4 5 6 7 8 9 10'
+print(my_string)
+
+
+for all in my_string:
+    print(all)
+    
+    
+    
